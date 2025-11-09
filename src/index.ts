48342c16439cc3ffd1e78ba11724101c283f38f6
heveidndksjd <kayoonkim@naver.com>
2025-11-09 22:42:47 +0900

    Feat: 스택 관리 기능 CRUD

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from 'express';
 import { projectRouter } from './modules/projects/project.routes';
 import { careerRouter } from './modules/careers/career.routes';
 import { licenseRouter } from './modules/licenses/license.routes';
+import { stackRouter } from './modules/stacks/stack.routes';
 
 
 const app = express();
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use('/api', projectRouter);
 app.use('/api', careerRouter);
 app.use('/api', licenseRouter);
+app.use('/api', stackRouter);
 
 // GET 요청이 '/' 경로로 들어오면 실행
 app.get('/', (req: Request, res: Response) => {
